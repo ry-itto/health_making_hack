21a7894f4ead71f72cbfd238140c7567aef3f76f
ryoya_ito <horusu.dqx@gmail.com>
2018-12-16 01:23:20 +0900

    use xib instead of storyboard

--- HealthMakingHack/AppDelegate.swift
+++ HealthMakingHack/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let mainViewController = MainViewController(nibName: "MainViewController", bundle: nil)
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = mainViewController
+        self.window = window
+        self.window?.makeKeyAndVisible()
+
         return true
     }
 
---
+++ HealthMakingHack/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  HealthMakingHack
+//
+//  Created by 伊藤凌也 on 2018/12/16.
+//  Copyright © 2018 uoa_RLS. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
